e6bd0acfccf38178ba44d987b82f2d2274791b0f
JoshD <joshuajdickerson@gmail.com>
2012-03-31 13:20:39 -0400

    I pulled all of the extras out of the ExampleControlProgram, and got to the meat of controlling movement. I added an if-loop on line 49 that uses the toc(tic) function to turn the robot after 8seconds

---
+++ JoshConfig1.m
@@ -0,0 +1,90 @@
+function finalRad= ExampleControlProgram(serPort)
+% Simple program for autonomously control the iRobot Create on either the
+% physical Create or the simulated version. This will simply spiral outward
+% and turn away from obstacles that detects with the bump sensors.
+%
+% For the physical Create only, it is assumed that the function call
+% serPort= RoombaInit(comPort) was done prior to running this program.
+% Calling RoombaInit is unnecessary if using the simulator.
+%
+% Input:
+% serPort - Serial port object, used for communicating over bluetooth
+%
+% Output:
+% finalRad - Double, final turning radius of the Create (m)
+
+% ExampleControlProgram.m
+% Copyright (C) 2011 Cornell University
+% This code is released under the open-source BSD license.  A copy of this
+% license should be provided with the software.  If not, email:
+% CreateMatlabSim@gmail.com
+
+    % Set constants for this program
+    maxDuration= 1200;  % Max time to allow the program to run (s)
+    maxDistSansBump= 5; % Max distance to travel without obstacles (m)
+    maxFwdVel= 0.5;     % Max allowable forward velocity with no angular 
+                        % velocity at the time (m/s)
+    maxVelIncr= 0.005;  % Max incrementation of forward velocity (m/s)
+    maxOdomAng= pi/4;   % Max angle to move around a circle before 
+                        % increasing the turning radius (rad)
+    
+    
+    % Initialize loop variables
+    tStart= tic;        % Time limit marker
+    distSansBump= 0;    % Distance traveled without hitting obstacles (m)
+    angTurned= 0;       % Angle turned since turning radius increase (rad)
+    v= 0.3;               % Forward velocity (m/s)
+    w= 0.0;          % Angular velocity (rad/s)
+    
+    % Start robot moving
+    SetFwdVelAngVelCreate(serPort,v,w)
+    
+    % Enter main loop
+    while toc(tStart) < maxDuration && distSansBump <= maxDistSansBump
+        distSansBump= distSansBump+DistanceSensorRoomba(serPort);
+        angTurned= angTurned+AngleSensorRoomba(serPort);
+        if toc(tStart) >= 8
+            w = 0.4;
+            SetFwdVelAngVelCreate(serPort,v,w)
+        end
+        % Briefly pause to avoid continuous loop iteration
+        pause(0.1)
+    end
+    
+    % Specify output parameter
+    finalRad= v/w;
+    
+    % Stop robot motion
+    v= 0;
+    w= 0;
+    SetFwdVelAngVelCreate(serPort,v,w)
+     
+%     % If you call RoombaInit inside the control program, this would be a
+%     % good place to clean up the serial port with...
+%     % fclose(serPort)
+%     % delete(serPort)
+%     % clear(serPort)
+%     % Don't use these if you call RoombaInit prior to the control program
+end
+
+
+    
+
+
+
+function w= v2w(v)
+% Calculate the maximum allowable angular velocity from the linear velocity
+%
+% Input:
+% v - Forward velocity of Create (m/s)
+%
+% Output:
+% w - Angular velocity of Create (rad/s)
+    
+    % Robot constants
+    maxWheelVel= 2.5;   % Max linear velocity of each drive wheel (m/s)
+    robotRadius= 0.6;   % Radius of the robot (m)
+    
+    % Max velocity combinations obey rule v+wr <= v_max
+    w= (maxWheelVel-v)/robotRadius;
+end
